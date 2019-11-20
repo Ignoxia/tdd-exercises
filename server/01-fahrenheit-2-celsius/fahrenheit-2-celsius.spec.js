e3af885ffe4a211f9ae29adc455a2e376c3e7edd
bjhartwe@us.ibm.com <bjhartwe@oc1653163806.ibm.com>
2019-11-20 12:33:54 -0500

    fahrenheit 2 celsius

--- server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,5 +1,19 @@
+let farenheit2celsius = fahrenheit => (fahrenheit -32) * 5 / 9;
+
 describe('the fahrenheit-2-celcius canary spec', () => {
   it('shows the infrastructure works', () => {
     expect(true).toBe(true);
   });
+  it('farenheit2celsius of 32 is 0', () => {
+    expect(farenheit2celsius(32)).toBe(0);
+  });
+    it('farenheit2celsius of 212 is 100', () => {
+      expect(farenheit2celsius(212)).toBe(100);
+    });
+    it('farenheit2celsius of 50 is 10', () =>{
+      expect(farenheit2celsius(50)).toBe(10);
+    },);
+    it('farenheit2celsius of -40 is -40', () =>{
+      expect(farenheit2celsius(-40)).toBe(-40);
+    },);
 });
