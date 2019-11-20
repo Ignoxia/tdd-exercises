e6223f0390824423aaaa442b46f2edf6bc300b64
bjhartwe@us.ibm.com <bjhartwe@oc1653163806.ibm.com>
2019-11-20 14:15:39 -0500

    palindrome

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,3 +1,35 @@
+let isPalindrome = (string) => {
+    if(!string || string.trim().length === 0) return false;
+
+    return string.replace(/\W+/g, '') === string.split('').reverse().join('').replace(/\W+/g, '');
+
+};
+
 describe('a palindrome function', () => {
-    it('canary shows the infrastructure works');
+    it('canary shows the infrastructure works',() => {
+        expect(true).toBe(true);
+    });
+
+    it('radar is a palindrome', () => {
+        expect(isPalindrome('radar')).toBe(true);
+    });
+
+    it('dog is not a palindrome', () => {
+        expect(isPalindrome('dog')).toBe(false);
+    });
+    it('race car is a palindrome', () => {
+        expect(isPalindrome('race car')).toBe(true);
+    });
+
+    it('mom dad mom is a palindrome');
+        expect(isPalindrome('mom dad mom')).toBe(true);
+
+    it('"" is not a palindrome');
+        expect(isPalindrome('')).toBe(false);
+
+    it('"   " is not a palindrome');
+        expect(isPalindrome('  ')).toBe(false);
+
+    it('undefined is not a polindrome');
+        expect(isPalindrome()).toBe(false);
 });
