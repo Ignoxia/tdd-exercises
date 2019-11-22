357f47d886e0c1e4f8c5ffb8c533da31d75cddda
bjhartwe@us.ibm.com <bjhartwe@oc1653163806.ibm.com>
2019-11-22 15:28:24 -0500

    martin fowler refactoring

--- server/05-martin-fowler-movie-example/Movie.js
+++ server/05-martin-fowler-movie-example/Movie.js
@@ -5,13 +5,47 @@ const CHILDRENS = 2;
 let Movie = (title, somePriceCode) => {
   let priceCode = somePriceCode;
 
+  function getCharge(daysRented) {
+    let subTotal = 0;
+    switch (priceCode) {
+      case REGULAR:
+        subTotal += 2;
+        if (daysRented > 2) {
+          subTotal += (daysRented - 2) * 1.5;
+        }
+        break;
+      case NEW_RELEASE:
+        subTotal += daysRented * 3;
+        break;
+      case CHILDRENS:
+        subTotal += 1.5;
+        if (daysRented > 3) {
+          subTotal += (daysRented - 3) * 1.5;
+        }
+        break;
+      default:
+    }
+    return subTotal;
+  }
+
+  function getFrequentRenterPoints(daysRented){
+    let frequentRenterPoints = 1;
+    // add bonus for a day new release rental
+    if((priceCode === NEW_RELEASE) &&
+      daysRented > 1)
+      frequentRenterPoints++;
+    return frequentRenterPoints;
+  }
+
   return {
     get title() { return title; },
     get priceCode() { return priceCode; },
     set priceCode(code) { priceCode = code; },
     REGULAR,
     NEW_RELEASE,
-    CHILDRENS
+    CHILDRENS,
+    getCharge,
+    getFrequentRenterPoints
   };
 };
 
@@ -19,4 +53,4 @@ Movie.REGULAR = REGULAR
 Movie.NEW_RELEASE = NEW_RELEASE
 Movie.CHILDRENS = CHILDRENS
 
-module.exports = {Movie};
+module.exports = { Movie };
--- server/05-martin-fowler-movie-example/Rental.js
+++ server/05-martin-fowler-movie-example/Rental.js
@@ -3,35 +3,11 @@ const {Movie} = require('./Movie')
 let Rental = (movie, daysRented) => {
 
   function getCharge() {
-    let subTotal = 0;
-    switch (movie.priceCode) {
-      case Movie.REGULAR:
-        subTotal += 2;
-        if (daysRented > 2) {
-          subTotal += (daysRented - 2) * 1.5;
-        }
-        break;
-      case Movie.NEW_RELEASE:
-        subTotal += daysRented * 3;
-        break;
-      case Movie.CHILDRENS:
-        subTotal += 1.5;
-        if (daysRented > 3) {
-          subTotal += (daysRented - 3) * 1.5;
-        }
-        break;
-      default:
-    }
-    return subTotal;
+    return movie.getCharge(daysRented);
   }
 
   function getFrequentRenterPoints(){
-    let frequentRenterPoints = 1;
-    // add bonus for a day new release rental
-    if((movie.priceCode === Movie.NEW_RELEASE) &&
-      daysRented > 1)
-      frequentRenterPoints++;
-    return frequentRenterPoints;
+    return movie.getFrequentRenterPoints(daysRented);
   }
 
   return {
@@ -43,3 +19,4 @@ let Rental = (movie, daysRented) => {
 };
 
 module.exports = { Rental };
+
